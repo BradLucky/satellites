5adb6484fb0efbf73538328b7ec4ae0b2fbb05f3
Brad Luczywo <BradLucky@cheezer.com>
2020-02-22 22:41:31 -0500

    First pass at metrics

--- __init__.py
+++ __init__.py
@@ -1,16 +1,79 @@
-from flask import Flask
-from sqlalchemy import create_engine
+from flask import Flask, jsonify
+
+from config import Config
+from models import init_db
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db_session = init_db(app)
 
 
 @app.route('/')
 def dashboard():
-    return 'Hello, World!'
+    sql = """
+        select
+            min(measurement_dt) min,
+            max(measurement_dt) max,
+            max(measurement_dt) - min(measurement_dt) total,
+            s.name
+        from
+            satellite_data sd
+            inner join satellites s on sd.satellite_id = s.id
+        group by
+            sd.satellite_id
+    """
+    q = db_session.execute(sql)
+    results = q.fetchall()
+    output = '\n'.join([f'{m.name}: {m.total}' for m in results])
+    return jsonify(output)
+
+
+@app.route('/task2/spot7')
+def task2():
+    sql = """
+        select
+            min(ionosphere) min_iono,
+            max(ionosphere) max_iono,
+            min(ndvi) min_ndvi,
+            max(ndvi) max_ndvi,
+            min(radiation) min_radiation,
+            max(radiation) max_radiation,
+            s.name
+        from
+            satellite_data sd
+            inner join satellites s on sd.satellite_id = s.id
+        where
+            s.name = 'SPOT7'
+        group by
+            sd.satellite_id
+    """
+    q = db_session.execute(sql)
+    results = q.fetchall()
+    output = '\n'.join([f'{m.name}: IONO-{m.min_iono}/{m.max_iono}' for m in results])
+    return jsonify(output)
+
+
+@app.route('/task3')
+def task3():
+    sql = """
+        select
+            avg(ionosphere) avg_iono,
+            avg(ndvi) avg_ndvi,
+            avg(radiation) avg_radiation,
+            s.name
+        from
+            satellite_data sd
+            inner join satellites s on sd.satellite_id = s.id
+        group by
+            sd.satellite_id
+    """
+    q = db_session.execute(sql)
+    results = q.fetchall()
+    output = '\n'.join([f'{m.name}: IONO-{m.avg_iono}' for m in results])
+    return jsonify(output)
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-    engine = create_engine('mysql+pymysql://sat:123@172.17.0.1:3306/satellites', echo=True)
-    connection = engine.connect()
